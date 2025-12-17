4dea00322fef6cf2496ee6ba18286a193c1a4db1
omerakpul <omerakpul38@gmail.com>
2025-12-18 01:57:15 +0300

    feat: Add Hilt module for database and repository
    - Create `AppModule.kt` to provide dependencies using Hilt.
    - Add a provider for the Room database (`PdfDatabase`).
    - Add a provider for the `PdfDao`.
    - Add a provider for the `PdfRepository` implementation.

---
+++ app/src/main/java/com/omerakpul/localpdf/di/AppModule.kt
@@ -0,0 +1,40 @@
+package com.omerakpul.localpdf.di
+
+import android.content.Context
+import androidx.room.Room
+import com.omerakpul.localpdf.data.local.dao.PdfDao
+import com.omerakpul.localpdf.data.local.database.PdfDatabase
+import com.omerakpul.localpdf.data.repository.PdfRepositoryImpl
+import com.omerakpul.localpdf.domain.repository.PdfRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+    @Provides
+    @Singleton
+    fun provideDatabase(@ApplicationContext context: Context) : PdfDatabase {
+        return Room.databaseBuilder(
+            context,
+            PdfDatabase::class.java,
+            "pdf_database"
+        ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideDao(db: PdfDatabase): PdfDao {
+        return db.pdfDao()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRepository(dao: PdfDao): PdfRepository {
+        return PdfRepositoryImpl(dao)
+    }
+}
