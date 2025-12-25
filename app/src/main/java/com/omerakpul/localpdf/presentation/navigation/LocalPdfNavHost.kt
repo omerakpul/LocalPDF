c335b54e0334117c3c719dff0ae6aba62008eceb
omerakpul <omerakpul38@gmail.com>
2025-12-25 16:18:48 +0300

    feat: Add navigation host structure
    - Create `LocalPdfNavHost.kt` with a `NavHost` composable.
    - Set up routes for `Splash`, `Home`, `Files`, `Detail`, `Merge`, `Split`, `Compress`, `Convert`, `PhotoToPdf`, `Sign`, and `Settings`.
    - Use type-safe navigation with `toRoute()` for the `Detail` destination.

---
+++ app/src/main/java/com/omerakpul/localpdf/presentation/navigation/LocalPdfNavHost.kt
@@ -0,0 +1,61 @@
+package com.omerakpul.localpdf.presentation.navigation
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.toRoute
+
+@Composable
+fun LocalPdfNavHost() {
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = Splash
+    ) {
+        composable<Splash> {
+            // TODO: SplashScreen()
+        }
+
+        composable<Home> {
+            // TODO: HomeScreen()
+        }
+
+        composable<Files> {
+            // TODO: FilesScreen()
+        }
+
+        composable<Detail> { backStackEntry ->
+            val detail: Detail = backStackEntry.toRoute()
+            // TODO: DetailScreen(pdfId = detail.pdfId)
+        }
+
+        composable<Merge> {
+            // TODO: MergeScreen()
+        }
+
+        composable<Split> {
+            // TODO: SplitScreen()
+        }
+
+        composable<Compress> {
+            // TODO: CompressScreen()
+        }
+
+        composable<Convert> {
+            // TODO: ConvertScreen()
+        }
+
+        composable<PhotoToPdf> {
+            // TODO: PhotoToPdfScreen()
+        }
+
+        composable<Sign> {
+            // TODO: SignScreen()
+        }
+
+        composable<Settings> {
+            // TODO: SettingsScreen()
+        }
+    }
+}
